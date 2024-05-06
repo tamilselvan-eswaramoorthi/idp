9540c4b743c9f8873990d2d279dbdd0096dfb82c
tamilselvan-eswaramoorthi <eswaramoorthi.tamilselvan@gmail.com>
2024-05-06 22:17:56 +0530

    w2 form parser

---
+++ application/tools/parser/inference.py
@@ -0,0 +1,33 @@
+from transformers import AutoTokenizer, LayoutLMForQuestionAnswering
+from datasets import load_dataset
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("impira/layoutlm-document-qa", add_prefix_space=True)
+model = LayoutLMForQuestionAnswering.from_pretrained("impira/layoutlm-document-qa", revision="1e3ebac")
+
+dataset = load_dataset("nielsr/funsd", split="train")
+example = dataset[0]
+question = "what's his name?"
+words = example["words"]
+boxes = example["bboxes"]
+
+encoding = tokenizer(
+    question.split(), words, is_split_into_words=True, return_token_type_ids=True, return_tensors="pt"
+)
+bbox = []
+for i, s, w in zip(encoding.input_ids[0], encoding.sequence_ids(0), encoding.word_ids(0)):
+    if s == 1:
+        bbox.append(boxes[w])
+    elif i == tokenizer.sep_token_id:
+        bbox.append([1000] * 4)
+    else:
+        bbox.append([0] * 4)
+encoding["bbox"] = torch.tensor([bbox])
+
+word_ids = encoding.word_ids(0)
+outputs = model(**encoding)
+loss = outputs.loss
+start_scores = outputs.start_logits
+end_scores = outputs.end_logits
+start, end = word_ids[start_scores.argmax(-1)], word_ids[end_scores.argmax(-1)]
+print(" ".join(words[start : end + 1]))
